cb4262f74614681af3dc16929246ba86c4d80572
Kiran <131623766+KiranLearner@users.noreply.github.com>
2024-02-29 22:47:03 -0500

    Add files via upload

---
+++ Logical_operator.js
@@ -0,0 +1,15 @@
+// jonas schmidtman course from Udemy
+// Challenge 3 
+
+let scoreDol = [96+108+89];
+let scoreKo =[88+91+110];
+let scoreDolphins =scoreDol/3;
+let scoreKoalas = scoreKo/3;
+
+if (scoreDolphins>scoreKoalas){
+    console.log("Dolphins win the trophy")
+} else if (scoreKoalas>scoreDolphins) {
+    console.log("Koalas win the trophy")
+} else if( scoreKoalas === scoreDolphins){
+    console.log("Both win the trophy")
+}
